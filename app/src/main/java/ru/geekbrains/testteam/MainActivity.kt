7c05f5b5b800689f1f008a068397412ab257674d
DwarfPorter <mrvlmor@gmail.com>
2023-02-15 21:29:42 +0300

    Новая задача

--- app/src/main/java/ru/geekbrains/testteam/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/testteam/MainActivity.kt
@@ -2,11 +2,15 @@ package ru.geekbrains.testteam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /////////Создать спринт 1
+
+        var a = 4
+        Toast.makeText(this, "aaa" + a.toString(), Toast.LENGTH_SHORT).show()
+
     }
 }
