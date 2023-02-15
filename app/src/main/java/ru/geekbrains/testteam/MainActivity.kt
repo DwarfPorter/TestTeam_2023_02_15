c67f2c867e78a497317aabcc80a81144f483094a
Vladimir Morozov <mrvlmor@gmail.com>
2023-02-15 21:31:17 +0300

    обработка кнопки (#3)

--- app/src/main/java/ru/geekbrains/testteam/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/testteam/MainActivity.kt
@@ -2,11 +2,19 @@ package ru.geekbrains.testteam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /////////Создать спринт 1
+
+        var btn = findViewById<Button>(R.id.button);
+        btn.setOnClickListener {
+            Toast.makeText(this, "Button", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
